c11432b5cc56a5421698efd316b76d4aaa89049a
Cant Code <95113225+CantCode023@users.noreply.github.com>
2021-12-21 15:47:22 +0000

    added some javascript and css. website is done.

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello World!")
+const countEl = document.getElementById("count")
+
+updateVisitCount()
+
+function updateVisitCount() {
+    fetch("https://api.countapi.xyz/update/visitcounter/cantcode?amount=1")
+        .then(response => response.json())
+        .then(response => {
+            countEl.innerHTML = response.value
+        })
+}
